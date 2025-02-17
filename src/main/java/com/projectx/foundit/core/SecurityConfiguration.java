6f994b20cc90b18e8d007151f97bd3c4613b63da
Tharuka Dananjana <32910424+tharuka95@users.noreply.github.com>
2025-02-17 12:51:01 -0500

    change cors

--- src/main/java/com/projectx/foundit/core/SecurityConfiguration.java
+++ src/main/java/com/projectx/foundit/core/SecurityConfiguration.java
@@ -44,7 +44,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8081", "http://backend.ca"));
+        configuration.setAllowedOrigins(List.of("*","http://localhost:8081", "http://backend.ca"));
         configuration.setAllowedMethods(List.of("GET", "POST","PUT", "DELETE" ));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 
